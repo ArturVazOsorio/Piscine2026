ed33c466ad8bd84c32bf353eb56725c7a975d5cf
Artur Vaz Osorio <arturvitorantelavazosorio@gmail.com>
2026-01-28 05:02:36 +0000

    adding ex04

---
+++ Shell01/ex04/MAC.sh
@@ -0,0 +1,8 @@
+ifconfig | grep "ether " | awk '{print $2}'
+
+
+ifconfig lista todo dispositos de conecao com internet
+|
+grep "ether " filtra apenas ether lista que contem enderecos mac
+|
+akw '{print $2}' imprime apenas segunda coluna resultante do grep
